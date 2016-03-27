2344c8bd06d7fb8c2d1d5bc73f6cecda8e18f795
rharrisUNCC <rharri92@uncc.edu>
2016-03-27 14:10:15 -0400

    Create database_db.php
    Testing to see if this works.

---
+++ database_db.php
@@ -0,0 +1,13 @@
+<php?>
+CREATE TABLE user
+(
+first_name VARCHAR(15) NOT NULL,
+last_name CHAR NOT NULL,
+age INT NOT NULL,
+city VARCHAR(25) NOT NULL,
+major VARCHAR(30) NOT NULL,
+email_address VARCHAR(30) NOT NULL,
+username VARCHAR(20) NOT NULL,
+password VARCHAR(15) NOT NULL
+);
+<?>
