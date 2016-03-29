bd019a2105b5d4b0f311c20f58873fc09f7a58b0
rharrisUNCC <rharri92@uncc.edu>
2016-03-29 16:34:16 -0400

    Create registration_data.php

---
+++ registration_data.php
@@ -0,0 +1,25 @@
+<?php
+  $firstName = $_POST['firstName'];
+  $lastName = $_POST['lastName'];
+  $age = $_POST['age'];
+  $city = $_POST['city'];
+  $major = $_POST['major'];
+  $email = $_POST['email'];
+  $username = $_POST['username'];
+  $password = $_POST['password'];
+  $aboutUser = $_POST['aboutUser'];
+
+    if (empty($first_name)) { {
+      } if (empty($last_name)) {
+        } if (empty($city)) {
+          } if (empty($major)) {
+            } if (empty($major)) {
+              } if (empty($email)) {
+                } if (empty($username)) {
+                  } if (empty($password)) {
+                    } if (empty($aboutUser)) {
+                      error_message = 'Field(s) were left empty!'
+                    }
+                    }
+
+?>
