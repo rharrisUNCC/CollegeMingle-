cd1dc738269a803af5da2883ce0a5530be3fe490
rharrisUNCC <rharri92@uncc.edu>
2016-04-07 14:27:26 -0400

    Update user_db.php

--- model/user_db.php
+++ model/user_db.php
@@ -10,13 +10,25 @@ function add_user() {
                 (:user_id, :user_sex, :wanted_sex, :first_name, :last_name, :age, :city
                 :state, :college, :major, :year, :email, :username, :pass, :bio, :user_type, :wanted_type)';
   $statement = $db->prepare($query);
-  $statement->bindValue(':', $);
-  $statement->bindValue(':', $);
-  $statement->bindValue(':', $);
-  $statement->bindValue(':', $);
+  $statement->bindValue(':user_id', $user_id);
+  $statement->bindValue(':user_sex', $user_sex);
+  $statement->bindValue(':wanted_sex', $wanted_sex);
+  $statement->bindValue(':first_name', $first_name);
+  $statement->bindValue(':last_name', $last_name);
+  $statement->bindValue(':age', $age);
+  $statement->bindValue(':city', $city);
+  $statement->bindValue(':state', $state);
+  $statement->bindValue(':college', $college);
+  $statement->bindValue(':major', $major);
+  $statement->bindValue(':year', $year);
+  $statement->bindValue(':email', $email);
+  $statement->bindValue(':username', $username);
+  $statement->bindValue(':pass', $pass);
+  $statement->bindValue(':bio', $bio);
+  $statement->bindValue(':user_type', $user_type);
+  $statement->bindValue(':wanted_type', $wanted_type);
   $statement->execute();
   $statement->closeCursor();
-  return $users;
 }
 //delete_user function
 function delete_user() {
